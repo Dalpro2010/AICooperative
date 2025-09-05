95aac1b999da83c4803e4a75dc667168bc6d27f7
Daniel Esteban melero <danielestebanmelero2000@gmail.com>
2025-09-05 09:40:00 +0000

    quiero que el icono de la luna en el moso oscuro este bien puesto a la i

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -30,8 +30,11 @@ export default function ThemeToggle() {
       className="w-full justify-center group-data-[collapsible=icon]:justify-center"
       aria-label="Toggle theme"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      {theme === 'light' ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
       <span className="group-data-[collapsible=icon]:hidden ml-2">
         {theme === 'light' ? 'Modo Oscuro' : 'Modo Claro'}
       </span>
